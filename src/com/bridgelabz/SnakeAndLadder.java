d4b4bcb6441fe6c5d173d4fe2f37e5057826e921
bhure01 <dattatribhure@gmail.com>
2022-01-09 17:25:46 +0530

    Modified: player rolling dice to get numbers betn 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,13 +4,22 @@ public class SnakeAndLadder {
     int playerInitialPosition = 0;
 
     private void showPlayerPosition() {        //showing current position
-        System.out.println("Player Starting Position = "+ playerInitialPosition);
+        System.out.println("Player Starting Position = " + playerInitialPosition);
+    }
+
+    private int rollDice() {            // roll the dice and get random no.betn 1 to 6
+        int diceNo = (int) Math.floor((Math.random() * 10) % 6 + 1);
+        return diceNo;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Gaming program");
         SnakeAndLadder player1 = new SnakeAndLadder();
+        player1.showPlayerPosition(); //show player position
+        player1.rollDice();
 
-        player1.showPlayerPosition();     //show player position
+        System.out.println("Dice no. for Player is: " +player1.rollDice());
     }
 }
+
+
